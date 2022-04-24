8661c83a87e18abc5860d07b36de2494efe9223a
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-24 23:28:07 +0800

    Add game class

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -1,6 +1,11 @@
 require './player.rb'
 require './tic_tac_toe.rb'
 
-2.times { Player.create }
+class Game
+  def initialize
+    2.times { Player.create }
+    TicTacToe.start_game(Player.list[0], Player.list[1])
+  end
+end
 
-TicTacToe.start_game(Player.list[0], Player.list[1])
+Game.new
