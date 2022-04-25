9bb16411e5122b24d4b852b84a520f6ad8c56d6b
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-25 13:07:58 +0800

    Add game class with play again functionality

--- wordle/game.rb
+++ wordle/game.rb
@@ -1,3 +1,37 @@
 require './wordle.rb'
+require './colorize.rb'
 
-Wordle.start_game
+class Game
+  include Colorize
+
+  def initialize
+    @stop = false
+    start
+
+    start until @stop
+  end
+
+  def start
+    Wordle.start_game
+    @stop = true unless play_again
+  end
+
+  def play_again
+    print 'Play again? (y/n) '
+    answer = gets.chomp
+    puts
+
+    until answer == 'y' || answer == 'n'
+      puts red('Invalid input.')
+      puts
+      print 'Play again? (y/n) '
+      answer = gets.chomp
+      puts
+    end
+
+    return true if answer == 'y'
+    return false if answer == 'n'
+  end
+end
+
+Game.new
