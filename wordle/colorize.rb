1e1e7b27527198f37a6a9cf37a37cd035c74196b
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-25 11:13:16 +0800

    Initial commit for wordle project

---
+++ wordle/colorize.rb
@@ -0,0 +1,15 @@
+require 'colorize'
+
+module Colorize
+  def green(string)
+    string.colorize(background: :light_green)
+  end
+
+  def yellow(string)
+    string.colorize(background: :light_yellow)
+  end
+
+  def gray(string)
+    string.colorize(background: :light_white)
+  end
+end
---
+++ wordle/wordle.rb
@@ -0,0 +1,5 @@
+require './colorize.rb'
+
+class Wordle
+  include Colorize
+end
