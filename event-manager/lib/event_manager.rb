897eef41fac0dec829e70e8a5d1d63e73adf9c88
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-11 21:09:26 +0800

    Use csv parser

--- event-manager/lib/event_manager.rb
+++ event-manager/lib/event_manager.rb
@@ -1,12 +1,22 @@
-puts 'Event Manager initialized!'
-puts
+require 'csv'
+
+def clean_zipcode(zipcode)
+  return '00000' if zipcode.nil?
+  return zipcode[0..4] if zipcode.length > 5
+  return zipcode.rjust(5, '0') if zipcode.length < 5
+  return zipcode
+end
+
+puts 'Event Manager initialized'
 
 if File.exists? 'event_attendees.csv'
-  file = File.open 'event_attendees.csv'
+  rows =
+    CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
-  while !file.eof?
-    line = file.readline
-    name = line.split(',')[2]
-    puts name
+  rows.each do |row|
+    first_name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+
+    puts "#{first_name} #{zipcode}"
   end
 end
