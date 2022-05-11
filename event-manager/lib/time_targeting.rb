7505f1d36434745ee0c67c9162ea90443ef11f1a
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-12 00:01:25 +0800

    Add time targeting

---
+++ event-manager/lib/time_targeting.rb
@@ -0,0 +1,30 @@
+require 'csv'
+require 'time'
+
+if File.exists? 'event_attendees.csv'
+  puts 'Starting process...'
+  puts
+
+  rows =
+    CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+
+  hours = rows.map { |row| Time.strptime(row[:regdate], '%m/%d/%y %k:%M').hour }
+
+  hash =
+    hours.reduce(Hash.new(0)) do |hash, hour|
+      hash[hour] += 1
+      hash
+    end
+
+  sorted = hash.sort_by { |key, value| value }.reverse
+
+  peak =
+    sorted
+      .select { |value| value.last == sorted.first.last }
+      .map { |value| value.first }
+
+  p peak
+
+  puts
+  puts 'Process finished'
+end
